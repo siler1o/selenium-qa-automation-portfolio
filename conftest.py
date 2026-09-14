eb28cf32ea6effeb7b6a2bd57e0a8b5400f22cb3
Rj <145338498+siler1o@users.noreply.github.com>
2026-09-14 16:07:56 +0800

    ci: support headless Chrome in GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
+import os
+
 import pytest
 from selenium import webdriver
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+
+    if os.getenv("CI"):
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Prevent third-party ads from interrupting practice-site tests
     driver.execute_cdp_cmd("Network.enable", {})
